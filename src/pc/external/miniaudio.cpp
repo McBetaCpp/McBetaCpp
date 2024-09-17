6393e1dc36a01391ee752d69ab491fd77656fe82
Regan Green <regan_green27@hotmail.com>
2024-09-17 08:37:00 -0400

    add miniaudio

---
+++ src/pc/external/miniaudio.cpp
@@ -0,0 +1,8 @@
+#include "external/stb_vorbis.h"
+
+#define MINIAUDIO_IMPLEMENTATION
+
+#define MA_NO_DEVICE_IO
+#define MA_NO_ENCODING
+
+#include "miniaudio.h"
---
+++ src/pc/external/stb_vorbis.cpp
@@ -0,0 +1,4 @@
+#include "stb_vorbis.h"
+
+#undef STB_VORBIS_HEADER_ONLY
+#include "stb_vorbis.c"
